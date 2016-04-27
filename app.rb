2ff28aa11f1914aadd7d9025df07a3feaaf58a89
Alex <alexv@Alexs-MacBook-Air.local>
2016-04-27 12:01:33 -0400

    small addition

--- app.rb
+++ app.rb
@@ -8,14 +8,17 @@ get '/' do
   erb :index
 end
 post '/location' do
+
+googleapi = ENV["google_geocoding_key"]
+uberapi = ENV["uber_server_token"]
 @location1 = params[:location1]
 @location2 = params[:location2]
-@googlelink = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyAizQCVgRDh-r19AJNTdX_XP3GsBV8uVk0&origin=#{@location1}&destination=#{@location2}&avoid=tolls|highways"
-uri = URI("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location1}&key=AIzaSyDxqqK4SVguPpCIH7fxD627TYS2iqRtHpY")
+@googlelink = "https://www.google.com/maps/embed/v1/directions?key=#{googleapi}&origin=#{@location1}&destination=#{@location2}&avoid=tolls|highways"
+uri = URI("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location1}&key=#{googleapi}")
 response = Net::HTTP.get_response(uri)
 response2 = JSON.parse(response.body)
 
-uri2 = URI("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location2}&key=AIzaSyDxqqK4SVguPpCIH7fxD627TYS2iqRtHpY")
+uri2 = URI("https://maps.googleapis.com/maps/api/geocode/json?address=#{@location2}&key=#{googleapi}")
 response3 = Net::HTTP.get_response(uri2)
 response4 = JSON.parse(response3.body)
   @lat1 = response2['results'][0]["geometry"]["location"]["lat"]
@@ -23,7 +26,7 @@ response4 = JSON.parse(response3.body)
   @lat2 = response4['results'][0]["geometry"]["location"]["lat"]
   @lng2 = response4['results'][0]["geometry"]["location"]["lng"]
 
-uri3 = URI("https://api.uber.com/v1/estimates/price?server_token=auRQ2fNlP5rXnlMKRVIT4rhHjcQWQjw79gCi_MOX&start_latitude=#{@lat1}&start_longitude=#{@lng1}&end_latitude=#{@lat2}&end_longitude=#{@lng2}")
+uri3 = URI("https://api.uber.com/v1/estimates/price?server_token=#{uberapi}&start_latitude=#{@lat1}&start_longitude=#{@lng1}&end_latitude=#{@lat2}&end_longitude=#{@lng2}")
 response5 = Net::HTTP.get_response(uri3)
 response6 = JSON.parse(response5.body)
 @fare = response6['prices'][1]['estimate']
@@ -32,11 +35,4 @@ response6 = JSON.parse(response5.body)
 @surge = response6['prices'][1]['surge_multiplier']
 
   erb :index
-end
-
-
-get '/hi' do
-  response = HTTParty.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDxqqK4SVguPpCIH7fxD627TYS2iqRtHpY')
-  puts response['results'][0]["geometry"]["location"]["lat"]
-  puts response['results'][0]["geometry"]["location"]["lng"]
 end
